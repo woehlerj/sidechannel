6bb111253a9b72ffc932d349170f9de23c83ede3
Ugur <ugurokms@gmail.com>
2024-05-27 15:16:08 +0900

    time.c changed

--- source/src/time.c
+++ source/src/time.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <err.h>
 
+
 // Override __stack_chk_fail to suppress stack smashing messages
 void __stack_chk_fail(void) {
     // Do nothing or handle the error as you wish
+    printf("Thank you for submitting feedback\n");
     exit(EXIT_FAILURE);
 }
-
 void start() {
   printf("Welcome to Cafe Grazie\n");
   printf("Please enter your feedback:\n");
@@ -17,8 +18,6 @@ void start() {
   char buf[32];
   memset(buf, 0, sizeof(buf));
   read(0, buf, 256);
-
-  printf("Thank you for submitting feedback\n");
   return;
 }
 
@@ -29,6 +28,7 @@ int main(int argc, char *argv[])
   setvbuf(stdin, NULL, _IONBF, 0);
 
   start();
-
+  usleep(500000);
+  printf("Thank you for submitting feedback\n");
   return 0;
 }
